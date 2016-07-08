e8e9c253058978f776fa08a387a76a1de3c60aad
Mark Laukkanen <mark.laukkanen@gmail.com>
2016-07-08 20:57:44 +0300

    Fixed issues with IBOutlet and IBAction name changes, finished creating main functionality

--- rpgoop/ViewController.swift
+++ rpgoop/ViewController.swift
@@ -12,21 +12,26 @@ class ViewController: UIViewController {
 
     
     @IBOutlet weak var printLbl: UILabel!
+    
     @IBOutlet weak var playerHpLbl: UILabel!
+    
     @IBOutlet weak var enemyHpLbl: UILabel!
+    
     @IBOutlet weak var enemyImg: UIImageView!
+    
     @IBOutlet weak var chestBtn: UIButton!
 
     var player: Player!
     var enemy: Enemy!
+    var chestMessage: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         player = Player(name: "MasterWarrior3000", hp: 110, attackPwr: 20)
-        playerHpLbl.text = "\(player.hp) HP"
-        
         generateRandomEnemy()
+        playerHpLbl.text = "\(player.hp) HP"
+    
     }
     
     func generateRandomEnemy() {
@@ -37,12 +42,36 @@ class ViewController: UIViewController {
         } else {
             enemy = WildWizard(startingHp: 60, attackPwr: 15)
         }
+        
+        enemyImg.hidden = false
     }
 
-    @IBAction func onChestTapped(sender: UIButton) {
+    @IBAction func onChestTapped(sender: AnyObject) {
+        chestBtn.hidden = true
+        printLbl.text = chestMessage
+        NSTimer.scheduledTimerWithTimeInterval(2.0, target: self, selector: #selector(ViewController.generateRandomEnemy), userInfo: nil, repeats: false)
     }
 
-    @IBAction func onAttackTapped(sender: UIButton) {
+    @IBAction func onAttackTapped(sender: AnyObject) {
+        
+        if enemy.attemptAttack(player.attackPwr) {
+            printLbl.text = "Attacked \(enemy.type) for \(player.attackPwr) HP!"
+            enemyHpLbl.text = "\(enemy.hp) HP"
+        } else {
+            printLbl.text = "Attack was unsuccessful!"
+        }
+        
+        if let loot = enemy.dropLoot() {
+            player.addItemToInventory(loot)
+            chestMessage = "\(player.name) found \(loot)"
+            chestBtn.hidden = false
+        }
+        
+        if !enemy.isAlive {
+            enemyHpLbl.text = ""
+            printLbl.text = "Killed \(enemy.type)!"
+            enemyImg.hidden = true
+        }
     }
 
 
