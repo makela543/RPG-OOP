e8db0a12acf53fd9a88107f5966b04bb897d7a14
Mark Laukkanen <mark.laukkanen@gmail.com>
2016-07-08 19:01:32 +0300

    Added code to generate a player and and an enemy on initial load

--- rpgoop/ViewController.swift
+++ rpgoop/ViewController.swift
@@ -17,10 +17,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var enemyImg: UIImageView!
     @IBOutlet weak var chestBtn: UIButton!
 
-    
+    var player: Player!
+    var enemy: Enemy!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        player = Player(name: "MasterWarrior3000", hp: 110, attackPwr: 20)
+        playerHpLbl.text = "\(player.hp) HP"
+        
+        generateRandomEnemy()
+    }
+    
+    func generateRandomEnemy() {
+        let rand = Int(arc4random_uniform(2))
+        
+        if rand == 0 {
+            enemy = Kimara(startingHp: 50, attackPwr: 12)
+        } else {
+            enemy = WildWizard(startingHp: 60, attackPwr: 15)
+        }
     }
 
     @IBAction func onChestTapped(sender: UIButton) {
