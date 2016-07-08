38dac8284502887876794adcbe7537b3febd0825
Mark Laukkanen <mark.laukkanen@gmail.com>
2016-07-08 17:54:36 +0300

    Created 'Kimara' subclass of 'Enemy'

---
+++ rpgoop/Kimara.swift
@@ -0,0 +1,30 @@
+//
+//  Kimara.swift
+//  rpgoop
+//
+//  Created by Mark Laukkanen on 08/07/16.
+//  Copyright © 2016 Mark Laukkanen. All rights reserved.
+//
+
+import Foundation
+
+class Kimara: Enemy {
+    
+    let IMMUNE_MAX = 15
+    
+    override var loot: [String] {
+        return ["Tough Hide", "Kimara Venom", "Flying Cow Skin", "Rare Trident"]
+    }
+    
+    override var type: String {
+        return "Kimara"
+    }
+    
+    override func attemptAttack(attackPwr: Int) -> Bool {
+        if attackPwr >= IMMUNE_MAX {
+            return super.attemptAttack(attackPwr)
+        } else {
+            return false
+        }
+    }
+}
